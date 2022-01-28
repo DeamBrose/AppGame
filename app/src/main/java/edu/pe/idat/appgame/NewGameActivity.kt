2319ecb2ca5fa5697c8828639d71360075849e3d
Mod <natalyortiz.401@gmail.com>
2022-01-28 15:39:10 -0500

    Diseño admin

---
+++ app/src/main/java/edu/pe/idat/appgame/NewGameActivity.kt
@@ -0,0 +1,15 @@
+package edu.pe.idat.appgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.pe.idat.appgame.databinding.ActivityNewGameBinding
+
+class NewGameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNewGameBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNewGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+    }
+}
